90cf31619671b3677dfb5a4293b43806cddca41f
maxeliwax <max.walldov@gmail.com>
2022-05-04 15:07:11 +0200

    wpm

--- TypeFast.py
+++ TypeFast.py
@@ -3,9 +3,17 @@
 #teinf-20
 #2022 VT
 
+import time
+
+
 def startmenu(strtmen):
+    strtmen.clear()
     strtmen.addstr("Welcome to TypeFast :-)")
     strtmen.addstr("press N to start a race")
 
 def display_text(strt, target, current, Wpm=0):
-    
+    strtmen.addstr(target)
+    strtmen.addstr(1, 0 , f"Wpm): {Wpm}")
+
+for i, char in enumerate(current):
+    current_char 
