6594f9158ed05437e03187cc2fef57d4f8003fbd
maxeliwax <max.walldov@gmail.com>
2022-05-17 22:47:31 +0200

    tog bort curses och skrev om typ allt

--- TypeFast.py
+++ TypeFast.py
@@ -3,45 +3,73 @@
 #teinf-20
 #2022 VT
 
-import curses
-from curses import color_pair, wrapper
+import time
+import random
 
-  
+def GetWordlist(f):# en funktion som läser in filen f och får tillbaka en lista med ord
+    a = open(f, "r", encoding="utf8")#https://www.w3schools.com/python/python_file_open.asp
+    return a.readlines()
 
-def main_meny(stdscr):                                                   
-    stdscr.clear()                                                         
-    stdscr.addstr(3, 45, "wellcome to FastType", curses.color_pair(1))    #från rad 11 till 26 ,38 till 39 källa Curses Programming with Python skriven av A.M. Kuchling, Eric S. Raymond https://docs.python.org/3/howto/curses.html
-    stdscr.addstr(5, 49,"\npress any key to begin", curses.color_pair(2))
-    stdscr.refresh()
-    key = stdscr.getkey()
-    print(key) #registerar vad du trycker på
 
-def wpm_test(stdscr):
-    target_sentense ="jag heter max"#en test text 
-    current_text = []
+def Welcome():
+    print()
+    print()
+    print("Welcome to Fasttype")
+    print()
 
-    stdscr.clear()
-    stdscr.addstr(10,50,target_sentense, curses.color_pair(2)) #skriver ut target_sentense på plats 10 vertical 50 horisontellt
-    stdscr.refresh()
-    stdscr.getkey()
-    while True:
-    
-   
+def Countdown(r):# nedräkning i r sekunder, https://www.programiz.com/python-programming/time
+    for t in range(r):#loop
+        a=r-t
+        print (str(a) + '...')
+        time.sleep(1)# väntar en sekund
+    print()
 
-    if c == 'q':
+
+def CheckWord(inp,wo): # jämför varje bokstav i två strängar, känd bugg jämför endast samma position, klarar inte förskjutning
+    l= min(len(inp),len(wo))# utgår från den kortaste strängen 
+    fel = 0
+    for t in range(l-1):
+       if inp[t] != wo[t]:
+            fel = fel+1
+    return fel
+
+def Summary(f,t,ws,wpm):
+    print ("--------------------------")
+    print ("Antal fel:" + str(fel))
+    print ("Tid:" + str(timed))
+    print ("Antal ord:" + str(ws))
+    print ("Ord per minut:" + str(wpm))
+    print ("--------------------------")
+
+
+#Här börjar programmet
+
+Words = GetWordlist("TypeThis.txt") #Hämtar lista med ord
+random.shuffle(Words)#slumpvis ordning på meningarna, https://www.w3schools.com/python/ref_random_shuffle.asp
+
+
+Welcome()
+
+
+#huvudloop
+for x in Words: #för varje mening i filen
+    input("Tryck enter")
+    Countdown(3)#nedräkning
+    print("Skriv:")
+    print(x)#skriv ut mening
+
+    timed = time.time()#spara undan tid 
+    text=input()#skriv in ord
+    timed = round(time.time() - timed, 2)#jämföra tiden med tiden man sparade undan
+
+    if text == '': #om inget skrivet lämna huvudloopen
         break
-
-def main(stdscr): #Källa från rad 34 till 37, rad 13 och 14  Programming in Color with ncurses gjord av Jim Hall on February 6, 2018 https://www.linuxjournal.com/content/programming-color-ncurses S
-
-    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_YELLOW) #detta skapar en bakrunds färg så det är  vit text och gul bakrund 
-    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLUE)
-    main_meny(stdscr)#kallar på funktion
-    wpm_test(stdscr)#kallar på funktion 
-
-
-
-
-
    
+    fel = CheckWord(text,x)#räkna fel
+    ws=len(text.split())# räkna antal ord
+    wpm= round((60/timed)*ws)#räkna ord per minut
+    Summary(fel,timed,ws,wpm)#skriva ut resultat
 
-wrapper(main)
+print()
+print("Tack för du spelat")
+
